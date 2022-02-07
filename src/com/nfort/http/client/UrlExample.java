9be4c739f85e7397c2ad1b4199a500eea5cf0401
Nadejda Doborina <NadejdaFort@gmail.com>
2022-02-07 23:50:50 +0300

    Servlets_12

---
+++ src/com/nfort/http/client/UrlExample.java
@@ -0,0 +1,27 @@
+package com.nfort.http.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class UrlExample {
+
+    public static void main(String[] args) throws IOException {
+        var url = new URL("file:/Users/nadejda_doborina/Documents/Java/DmDev/HTTP_Servlets/src/com/nfort/http/socket/DatagramRunner.java");
+        var urlConnection = url.openConnection();
+
+        System.out.println(new String(urlConnection.getInputStream().readAllBytes()));
+    }
+
+    private static void checkGoogle() throws IOException {
+        var url = new URL("https://www.google.com");
+        var urlConnection = url.openConnection();
+        urlConnection.setDoOutput(true);
+
+        try (var outputStream = urlConnection.getOutputStream()) {
+        }
+        System.out.println();
+    }
+}
