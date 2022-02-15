ca986fd1f38a06df22c81466f110fb6ff1dd0a54
Nadejda Doborina <NadejdaFort@gmail.com>
2022-02-15 14:21:08 +0300

    Servlets_18

---
+++ src/com/nfort/http/server/HttpServerRunner.java
@@ -0,0 +1,10 @@
+package com.nfort.http.server;
+
+public class HttpServerRunner {
+
+    public static void main(String[] args) {
+
+        var httpServer = new HttpServer(9000);
+        httpServer.run();
+    }
+}
